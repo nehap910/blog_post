6dd98b4e3f27e6a59699ccd43507834a76b81c25
nehap910 <pokharkarneha2017@gmail.com>
2024-07-17 14:24:36 +0530

    basic structure is ready

---
+++ BlogPost/src/copmonents/BlogPostList.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import axios from 'axios';
+import Grid from '@mui/material/Grid';
+import { useNavigate } from 'react-router-dom';
+function BlogPostList() {
+
+  const [news, setNews] = useState([]);
+  const navigate = useNavigate();
+
+  const [clickedCards, setClickedCards] = useState({});
+
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await axios.get('https://newsapi.org/v2/top-headlines', {
+          params: {
+            sources:"techcrunch",
+            apiKey: 'b9f74c0e8e7b4b7f87ce57c945cd2045',
+          },
+        });
+        setNews(response.data.articles);
+      } catch (error) {
+        console.error('Error fetching news:', error);
+      }
+    };
+
+    fetchNews();
+  }, []);
+
+  const handleCardClick = (index) => {
+    navigate(`/post/${index}`, { state: { article: news[index] } });
+  };
+  
+
+  return (
+  
+    <Grid container spacing={4} justifyContent="center">
+    {news.map((article, index) => (
+      <Grid item key={index} xs={12} sm={6} md={4}>
+        <Card
+          sx={{ maxWidth: 345, margin: '20px auto', cursor: 'pointer' }}
+          onClick={() => handleCardClick(index)}
+        >
+          <CardMedia
+            sx={{ height: 140 }}
+            image={article.urlToImage || '/static/images/cards/contemplative-reptile.jpg'}
+            title={article.title}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {article.title}
+            </Typography>
+          </CardContent>
+         
+        </Card>
+      </Grid>
+    ))}
+  </Grid>
+   
+  )
+}
+
+export default BlogPostList
